92dc78d0ee9653260eea283d3a9a7245f7201f67
kaiodeodato <kaiod.oficial@gmail.com>
2024-05-16 09:10:45 +0100

    fist commit

---
+++ ShakeBlazor/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Tailwind;
+using Blazorise.Icons.FontAwesome;
+using ShakeBlazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+    .AddGumtreeClient()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://nodegraphql-tog7.onrender.com/"))
+    .ConfigureWebSocketClient(c => c.Uri = new Uri("https://nodegraphql-tog7.onrender.com/"));
+
+builder.Services
+    .AddBlazorise()
+    .AddTailwindProviders()
+    .AddFontAwesomeIcons();
+
+await builder.Build().RunAsync();
+
